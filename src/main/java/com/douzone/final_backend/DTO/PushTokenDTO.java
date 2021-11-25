175565c28429c4389a14cf646ccc08d1eb9c66ce
KENKEN0803 <blackflamekr@gmail.com>
2021-11-25 18:33:54 +0900

    푸시서버 작업중

---
+++ src/main/java/com/douzone/final_backend/DTO/PushTokenDTO.java
@@ -0,0 +1,15 @@
+package com.douzone.final_backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PushTokenDTO {
+    private String g_owner_fk;
+    private String token;
+}
